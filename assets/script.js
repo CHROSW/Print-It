5ea190f5a5cfaa25ec1107d5ddf315e137b54e23
CHROSW <oschristophe@gmail.com>
2023-08-01 10:05:15 +0200

    Remplacement de innerText par innerHTML.

--- assets/script.js
+++ assets/script.js
@@ -51,8 +51,8 @@ switch(e.button){
 				var paragraphe = blocSlider.getElementsByTagName('p');
 				var pspan = document.createElement('span');
 				image[0].setAttribute('src', './assets/images/slideshow/' + slides[nbDots-1]['image']);
-				paragraphe[0].innerText = slides[nbDots-1]['tagLine'].substring(0, slides[nbDots-1]['tagLine'].indexOf('<'));
-				pspan.innerText = slides[nbDots-1]['tagLine'].substring(slides[nbDots-1]['tagLine'].indexOf('<span>')+6, slides[nbDots-1]['tagLine'].indexOf('</span>'));
+				paragraphe[0].innerHTML = slides[nbDots-1]['tagLine'].substring(0, slides[nbDots-1]['tagLine'].indexOf('<'));
+				pspan.innerHTML = slides[nbDots-1]['tagLine'].substring(slides[nbDots-1]['tagLine'].indexOf('<span>')+6, slides[nbDots-1]['tagLine'].indexOf('</span>'));
 				paragraphe[0].appendChild(pspan);
 			 }else{
 			 	listDot[nb].className = 'dot';
@@ -62,8 +62,8 @@ switch(e.button){
 				var paragraphe = blocSlider.getElementsByTagName('p');
 				var pspan = document.createElement('span');
 			 	image[0].setAttribute('src', './assets/images/slideshow/' + slides[nb-1]['image']);
-				paragraphe[0].innerText = slides[nb-1]['tagLine'].substring(0, slides[nb-1]['tagLine'].indexOf('<'));
-				pspan.innerText = slides[nb-1]['tagLine'].substring(slides[nb-1]['tagLine'].indexOf('<span>')+6, slides[nb-1]['tagLine'].indexOf('</span>'));
+				paragraphe[0].innerHTML = slides[nb-1]['tagLine'].substring(0, slides[nb-1]['tagLine'].indexOf('<'));
+				pspan.innerHTML = slides[nb-1]['tagLine'].substring(slides[nb-1]['tagLine'].indexOf('<span>')+6, slides[nb-1]['tagLine'].indexOf('</span>'));
 				paragraphe[0].appendChild(pspan);
 			 }
 	break;
@@ -92,8 +92,8 @@ switch(e.button){
 				var pspan = document.createElement('span');
 				console.log(paragraphe);
 				image[0].setAttribute('src', './assets/images/slideshow/' + slides[0]['image']);
-				paragraphe[0].innerText = slides[0]['tagLine'].substring(0, slides[0]['tagLine'].indexOf('<'));
-				pspan.innerText = slides[0]['tagLine'].substring(slides[0]['tagLine'].indexOf('<span>')+6, slides[0]['tagLine'].indexOf('</span>'));
+				paragraphe[0].innerHTML = slides[0]['tagLine'].substring(0, slides[0]['tagLine'].indexOf('<'));
+				pspan.innerHTML = slides[0]['tagLine'].substring(slides[0]['tagLine'].indexOf('<span>')+6, slides[0]['tagLine'].indexOf('</span>'));
 				paragraphe[0].appendChild(pspan);
 			 }else{
 			 	listDot[nb].className = 'dot';
@@ -104,8 +104,8 @@ switch(e.button){
 				var pspan = document.createElement('span');
 				console.log(paragraphe);
 			 	image[0].setAttribute('src', './assets/images/slideshow/' + slides[nb+1]['image']);
-				paragraphe[0].innerText = slides[nb+1]['tagLine'].substring(0, slides[nb+1]['tagLine'].indexOf('<'));
-				pspan.innerText = slides[nb+1]['tagLine'].substring(slides[nb+1]['tagLine'].indexOf('<span>')+6, slides[nb+1]['tagLine'].indexOf('</span>'));
+				paragraphe[0].innerHTML = slides[nb+1]['tagLine'].substring(0, slides[nb+1]['tagLine'].indexOf('<'));
+				pspan.innerHTML = slides[nb+1]['tagLine'].substring(slides[nb+1]['tagLine'].indexOf('<span>')+6, slides[nb+1]['tagLine'].indexOf('</span>'));
 				paragraphe[0].appendChild(pspan);
 			 }
 	break;
